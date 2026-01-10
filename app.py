7c67aa5fab024179d882290ef928c1a743505f69
emadjaradat <emadjaradat352@gmail.com>
2026-01-11 01:55:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,17 @@ from flask import Flask
 from flask_login import LoginManager
 from models import db
 from routes import main
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here'
-import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-app.config['SQLALCHEMY_DATABASE_URI'] = \
-    'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+
 login_manager = LoginManager()
 login_manager.login_view = 'main.login'
 login_manager.init_app(app)
@@ -26,30 +24,28 @@ def load_user(user_id):
 
 app.register_blueprint(main)
 
-from app import app
-from models import db
-import os
+
+def create_default_admin():
+    from models import User
+    from werkzeug.security import generate_password_hash
+
+    if not User.query.filter_by(username='admin').first():
+        hashed_password = generate_password_hash('admin123')
+        admin = User(
+            username='admin',
+            name='مدير النظام',
+            password=hashed_password,
+            role='manager'
+        )
+        db.session.add(admin)
+        db.session.commit()
+        print("Default admin created")
+
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-
-        # Create default manager if not exists
-        from models import User
-        from werkzeug.security import generate_password_hash
-
-        if not User.query.filter_by(username='admin').first():
-            hashed_password = generate_password_hash('admin123')
-            default_manager = User(
-                username='admin',
-                name='مدير النظام',
-                password=hashed_password,
-                role='manager'
-            )
-            db.session.add(default_manager)
-            db.session.commit()
-            print("Default manager created: username='admin', password='admin123'")
+        create_default_admin()
 
     port = int(os.environ.get("PORT", 5050))
-    app.run(host="0.0.0.0", port=port, debug=False)
-
+    app.run(host="0.0.0.0", port=port)
